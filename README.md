6cffbc8c756272de9167024dcc9544587c95b399
Loonride <22580625+Loonride@users.noreply.github.com>
2021-05-17 15:23:54 -0500

    add physics prop changes

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Team: Kir Nagaitsev, Rachel Wang, Sydney Jenkins, Alec Blagg
 
 ```
-roslaunch final_project turtlebot3_intro_robo_manipulation.launch
+roslaunch final_project multi_turtlebot3.launch
 roslaunch final_project action.launch
 ```
--- scripts/robot_movement.py
+++ scripts/robot_movement.py
@@ -1,30 +1,53 @@
 #!/usr/bin/env python3
 
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Vector3
 from std_srvs.srv import Empty
-from geometry_msgs.msg import Vector3
+from gazebo_msgs.msg import ODEPhysics
+from gazebo_msgs.srv import SetPhysicsProperties
 
 class RobotMovement(object):
     def __init__(self):
         rospy.init_node("robot_movement")
         self.initialized = False
 
-        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.cmd_vel_pub = rospy.Publisher('/tb3_0/cmd_vel', Twist, queue_size=10)
 
-        rospy.wait_for_service('/gazebo/reset_world')
-        reset = rospy.ServiceProxy('/gazebo/reset_world', Empty)
+        self.init_gazebo()
 
         rospy.sleep(2)
 
         print("Initialized")
         self.initialized = True
 
-        self.set_v(0, 0.5)
+        self.set_v(0.5, 0.5)
 
-        rospy.sleep(5)
-        reset()
+        rospy.sleep(60)
+        self.reset()
         print("Reset")
+
+
+    def init_gazebo(self):
+        rospy.wait_for_service('/gazebo/reset_world')
+        self.reset = rospy.ServiceProxy('/gazebo/reset_world', Empty)
+
+        rospy.wait_for_service('/gazebo/set_physics_properties')
+        set_physics_props = rospy.ServiceProxy('/gazebo/set_physics_properties', SetPhysicsProperties)
+
+        ode_config = ODEPhysics()
+        ode_config.auto_disable_bodies = False
+        ode_config.sor_pgs_precon_iters = 0
+        ode_config.sor_pgs_iters = 50
+        ode_config.sor_pgs_w = 1.3
+        ode_config.sor_pgs_rms_error_tol = 0.0
+        ode_config.contact_surface_layer = 0.001
+        ode_config.contact_max_correcting_vel = 100.0
+        ode_config.cfm = 0.0
+        ode_config.erp = 0.2
+        ode_config.max_contacts = 20
+        gravity = Vector3(0.0, 0.0, -9.8)
+
+        set_physics_props(0.002, 0.0, gravity, ode_config)
 
 
     def set_v(self, velocity, angular_velocity):
